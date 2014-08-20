77ab9a90d889d1c96107b83652d614185d74ac0a
albert <albert@albert-All-Series.(none)>
2014-08-20 18:23:38 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+// set html swig engine, similar to Jinja2
+var swig = require('swig');
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// set static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res){
+	res.render('index', {title: 'LoL Chat'})
+});
+
+io.on('connection', function (socket) {
+	console.log('New user connected');
+	socket.on('message', function (msg){
+		io.emit('alert.io', )
+	});
+});
+
+http.listen(3000, function (){
+  console.log('listening on *:3000');
+});
